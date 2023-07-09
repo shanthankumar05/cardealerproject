df62b60b57a3f54ad1c54cbdbcd0c6f4fca2c854
S.Shanthan Kumar <88592536+shanthankumar05@users.noreply.github.com>
2023-07-09 21:30:46 +0530

    Create Additional_Taxes.java

---
+++ Additional_Taxes.java
@@ -0,0 +1,11 @@
+
+public class Additional_Taxes {
+	private int rtocharges = 113990;
+	private int tcscharges = 11000;
+	public int getRtocharges() {
+		return rtocharges;
+	}
+	public int getTcscharges() {
+		return tcscharges;
+	}
+}
